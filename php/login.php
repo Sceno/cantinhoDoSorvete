19bbcd13a4e76f0db7d93eb9531980d6d8e18962
Diego Damasceno <diegodamasceno16@gmail.com>
2019-05-08 22:56:44 -0300

    Modificados: index.html // criados: php/home.php e php/login.php

--- php/login.php
+++ php/login.php
@@ -3,23 +3,24 @@
     
     include_once("conexao.php");
 
-    if(isset($_POST["email"]){
+    if(isset($_POST["email"])){
         $email = $_POST["email"];
         $senha = $_POST["senha"];
         
-        $comandoSQL = "SELECT * FROM usuarios ";
-        $comandoSQL .= "WHERE email = '{$email}' AND senha = '{$senha}";
+        $comandoSQL = "SELECT * FROM usuarios WHERE email = '{$email}' AND senha = '{$senha}'";
         
         $query = mysqli_query($conexao, $comandoSQL);
-        if($query){
+        if(!$query){
             echo "Falha na consulta ao banco de dados!";
         }else{
             while($usuario = mysqli_fetch_assoc($query)){
                 if($email != $usuario["email"]){
+                    header("location: ../index.html");
                     $mensagem = "Usuario ou senha inválidos!";
+                    echo $mensagem;
                 }else{
                     $_SESSION["usuario"] = $usuario["nome"];
-                    header("location: ../index.html");
+                    header("location: ../php/home.php");
                 }
             }
         }
